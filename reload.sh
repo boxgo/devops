0def574c47dc45a8e023f91f980bed871c803f86
amazing.gao <amazing.gao@qq.com>
2021-09-02 19:47:43 +0800

    feat(alert): alertmanager增加status字段分组；升级box告警规则

--- reload.sh
+++ reload.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 killall -s SIGHUP prometheus
-killall -s SIGHUP pushgateway
 killall -s SIGHUP alertmanager
-killall -s SIGHUP grafana-server
+# killall -s SIGHUP pushgateway
+# killall -s SIGHUP grafana-server
+
+docker ps
